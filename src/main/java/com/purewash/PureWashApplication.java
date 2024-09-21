dfb40446ce6c12fef850aa90adc88e65a9c7cbfc
ngntu10 <phamnguyentu04@gmail.com>
2024-09-21 21:18:23 +0700

    build: set up project

---
+++ src/main/java/com/purewash/PureWashApplication.java
@@ -0,0 +1,13 @@
+package com.purewash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureWashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PureWashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purewash/PureWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purewash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureWashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
